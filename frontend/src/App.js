ca42edd47918a7a7c16b3cb66df08a0011b024fb
revatidongare <revati@vinnovatetechnologies.com>
2020-10-09 23:02:32 +0530

    front end

---
+++ frontend/src/App.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props){
+  super(props);
+  this.state={
+    users:[],
+    id:0,
+    name:'',
+    email:'',
+    password:''
+  }
+}
+
+componentDidMount(){
+  axios.get('http://localhost:4000/api')
+  .then((res) => {
+    this.setState({
+      users:res.data,
+      id:0,
+      name:'',
+      email:'',
+      password:''
+    })
+    // console.log(data)
+  })
+}
+
+namechange = event =>{
+  this.setState({
+    name:event.target.value
+  })
+}
+emailchange = event =>{
+  this.setState({
+    email:event.target.value
+  })
+}
+passwordchange = event =>{
+  this.setState({
+    password:event.target.value
+  })
+}
+submit(event, id){
+  event.preventDefault()
+  if(id === 0){
+    axios.post('http://localhost:4000/api', {name:this.state.name, email:this.state.email, password:this.state.password})
+    .then(()=>{
+      this.componentDidMount()
+    })
+  }
+  else{
+    axios.put(`http://localhost:4000/api/${id}`, {name:this.state.name, email:this.state.email, password:this.state.password})
+    .then(()=>{
+      this.componentDidMount()
+    })
+  }
+}
+delete(id){
+  axios.delete(`http://localhost:4000/api/${id}`)
+  .then(()=>{
+    this.componentDidMount()
+  })
+}
+edit(id){
+  axios.get(`http://localhost:4000/api/${id}`)
+  .then((res)=>{
+    this.setState({
+      id:res.data._id,
+      name:res.data.name,
+      email:res.data.email,
+      password:res.data.password
+    })
+  })
+}
+  render(){
+  return (
+    <div className="row">
+      <div className="col s6">
+
+        <form onSubmit={(e)=>this.submit(e,this.state.id)}>
+        <div className="input-field col s12">
+          <i className="material-icons prefix">person</i>
+          <input value={this.state.name} onChange={(e)=>this.namechange(e)} type="text" id="autocomplete-input" class="autocomplete" />
+          <label htmlFor="autocomplete-input">Name</label>
+        </div>
+
+        <div className="input-field col s12">
+          <i className="material-icons prefix">mail</i>
+          <input value={this.state.email} onChange={(e)=>this.emailchange(e)} type="email" id="autocomplete-input" class="autocomplete" />
+          <label htmlFor="autocomplete-input">Email</label>
+        </div>
+
+        <div className="input-field col s12">
+          <i className="material-icons prefix">vpn_key</i>
+          <input value={this.state.password} onChange={(e)=>this.passwordchange(e)} type="password" id="autocomplete-input" class="autocomplete" />
+          <label htmlFor="autocomplete-input">Password</label>
+        </div>       
+  <button class="btn waves-effect waves-light right" type="submit" name="action">Submit
+    <i class="material-icons right">send</i>
+  </button> 
+        </form>
+        </div>
+        <div className="col s6">
+            
+      <table>
+        <thead>
+          <tr>
+          <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.state.users.map(user => 
+            <tr key={user._id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.password}</td>
+            <td>
+                <button onClick={(e)=>this.edit(user._id)} class="btn waves-effect waves-light" type="submit" name="action">
+                 <i class="material-icons">edit</i>
+                </button>
+            </td>
+            <td>
+                <button onClick={(e)=>this.delete(user._id)}class="btn waves-effect waves-light" type="submit" name="action">
+                 <i class="material-icons">delete</i>
+                </button>
+            </td>
+          </tr>
+          )}
+         </tbody>
+      </table>
+      </div>
+    </div>
+  );
+  }
+}
+
+export default App;
